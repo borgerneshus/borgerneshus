6b8fa330b9c9047fbd0709d4082da08fc54d1550
Tobias F. Petersen <tobsf@hotmail.com>
2018-06-13 13:48:33 +0200

    make possible to make full "page" pages

--- templates/node--page.tpl.php
+++ templates/node--page.tpl.php
@@ -82,8 +82,13 @@
 
 <div class="container-fluid">
     <div class="row well">
-
-        <div class="col-lg-9 col-md-8 col-sm-8 col-xs-12 col-xs-height col-full-height">
+        <?php 
+        $classes = "col-lg-9 col-md-8 col-sm-8 col-xs-12 col-xs-height col-full-height";
+        if($node->field_g_r_full_side[0]['value'] == 0) {
+            $classes = "col-lg-12 col-md-12 col-sm-12 col-xs-12 col-xs-height col-full-height";
+        }
+        ?>
+        <div class="<?php echo $classes ?>">
 
             <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
@@ -143,12 +148,13 @@
             </div>
 
         </div>
-
+        <?php if($node->field_g_r_full_side[0]['value'] == 0) {?>
         <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12 col-sm-height col-md-height col-xs-height col-full-height col-top hidden-print" style="padding-left:0;padding-right:0;">            
             <?php print render($content['field_title_image']); ?>
             <div class="info-field">
                 <p><?php print render($content['field_attachments']); ?></p>
             </div>
         </div>
+        <?php } ?>
     </div>
 </div>
