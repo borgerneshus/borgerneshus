1a44102948b101781eb97aad3af2164ccae3a3f4
Tobias F. Petersen <tobsf@hotmail.com>
2018-06-14 13:06:16 +0200

    iframe fun

--- js/oc_harry.js
+++ js/oc_harry.js
@@ -104,7 +104,7 @@ jQuery(document).ready(function(){
         jQuery('#gtranslate').modal('hide');
     });
         if ($('iframe').exists()) {
-            resizeIframe($('iframe'));
+           //resizeIframe($('iframe'));
            // $('iframe').attr('style', 'position: absolute; left: 0px; top: 0px; width: 90%; height: 100%');
            // $('iframe').parent().attr('style', 'position: relative; width: 100%; height: 0px; padding-bottom: 60%;');
         }
@@ -153,7 +153,8 @@ jQuery(document).ready(function(){
        var html = $('<div class="oc-bg-img-credit hidden-xs" >Foto: '+image_credit+'</div>');
        $('#footer').before(html);
     }
-    function resizeIframe(iframe) {
-    iframe.height = iframe.contentWindow.document.body.scrollHeight + "px";
-    }
+
  }(jQuery);
+ function resizeIframe(iframe) {
+    iframe.height = iframe.contentWindow.document.body.scrollHeight + "px";
+}
